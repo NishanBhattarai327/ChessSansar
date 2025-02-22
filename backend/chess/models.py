489db2ad2d8d8993e5af3b14142fda62f8cb95df
NishanBhattarai327 <bhattarainishan327@gmail.com>
2025-02-22 17:18:54 +0545

    One to one relation between clock and game

--- backend/chess/models.py
+++ backend/chess/models.py
@@ -2,42 +2,48 @@ from django.db import models
 import datetime
 from django.contrib.auth.models import User
 
-class Clock(models.Model):
-    started_at = models.DateTimeField(auto_now_add=True)
-    total_time = models.TimeField(default=datetime.timedelta(minutes=15))
-    incremental_time = models.TimeField(default=datetime.timedelta(seconds=0))
-    clock1 = models.TimeField(default=total_time)  # Clock for player1
-    clock2 = models.TimeField(default=total_time)  # Clock for player2
-
-
 class Game(models.Model):
-    player1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='player1_games')
-    player2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='player2_games')
+    player1 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='player1_games')
+    player2 = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='player2_games')
 
-    TURN_CHOICES = [('player1', player1), ('player2', player2)]
+    TURN_CHOICES = [('player1', 'Player 1'), ('player2', 'Player 2')]
     current_turn = models.CharField(max_length=64, choices=TURN_CHOICES, default='player1')
 
     fen = models.CharField(max_length=100, default='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')  # Initial FEN/ Board
     STATUS_CHOICES = [('waiting', 'Waiting'), ('active', 'Active'), ('ended', 'Ended')]
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='waiting')
-    winner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='won_games')
+
+    winner = models.CharField(max_length=64, choices=TURN_CHOICES, null=True, blank=True)
+
     OVER_TYPES = [('draw', 'Draw'), ('resign', 'Resign'), ('checkmate', 'Checkmate')]
-    over_type = models.CharField(max_length=20, null=True, choices=OVER_TYPES)
+    over_type = models.CharField(max_length=20, null=True, blank=True, choices=OVER_TYPES)
     
     # time
     FORMAT_CHOICES = [('classic', 'Classic'), ('rapid', 'Rapid'), ('bliz', 'Bliz'), ('bullet', 'Bullet'), ('custom', 'Custom')]
     format = models.CharField(max_length=10, choices=FORMAT_CHOICES, default='rapid')
-
-    clock = models.OneToOneField(
-        Clock,
-        on_delete=models.PROTECT,
-        related_name='game_clock',
-        null=True
-    )
     
     def __str__(self):
-        return f"Game {self.id}"
+        return f"Game {self.id} between {self.player1} and {self.player2} of format {self.format}"
 
+class Clock(models.Model):
+    DEFAULT_TOTAL_TIME = 15 * 60 * 1000  # 15 minutes in milliseconds
+    DEFAULT_INCREMENTAL_TIME = 0  # 0 seconds
+
+    game = models.OneToOneField(
+        Game,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    started_at = models.DateTimeField(auto_now_add=True)
+    total_time = models.IntegerField(default=DEFAULT_TOTAL_TIME, help_text="Total time in milliseconds")
+    incremental_time = models.IntegerField(default=DEFAULT_INCREMENTAL_TIME, help_text="Incremental time in milliseconds")
+
+    clock1 = models.IntegerField(default=DEFAULT_TOTAL_TIME, help_text="Player1 remaining time in ms")  # Clock for player1
+    clock2 = models.IntegerField(default=DEFAULT_TOTAL_TIME, help_text="Player2 remaining time in ms")  # Clock for player2
+
+    def __str__(self):
+        return f"Clock {self.total_time/(1000*60)}mins|+{self.incremental_time/1000}secs ({self.started_at}) game: {self.game}"
     
 class Move(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='game_moves')
@@ -45,4 +51,4 @@ class Move(models.Model):
     played_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Played move {self.move} in game {self.game.id}"
+        return f"Played move {self.move} in game {self.game}"
