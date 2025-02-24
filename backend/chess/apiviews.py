91480992fb45929289c61f89ab7d884fad56726e
NishanBhattarai327 <bhattarainishan327@gmail.com>
2025-02-24 22:46:13 +0545

    Add IsAuthenticated permission to get_games apiviews

--- backend/chess/apiviews.py
+++ backend/chess/apiviews.py
@@ -1,10 +1,12 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Game, Move
 from .serializers import GameSerializer
 
 @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def get_games(request):
     games = Game.objects.all()
     serializer = GameSerializer(games, many=True)
